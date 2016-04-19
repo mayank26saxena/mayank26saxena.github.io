8477685d5f5c96aeb7d82a5d919d0b1ce9d16c56
mayank26saxena <mayank26saxena@gmail.com>
2016-04-19 23:18:33 +0530

    Finding error.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,30 @@ layout: null
 ---
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) { console.log('In function 0.') return }
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
+	console.log('In function 1.')
       $('.panel-cover').addClass('panel-cover--collapsed')
       $('.content-wrapper').addClass('animated slideInRight')
     } else {
+	console.log('In function 1 else.')
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+
     }
   })
+
+  if (window.location.hash && window.location.hash == '#blog') {
+console.log('In function 2.')
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+console.log('In function 3.')
+    $('.panel-cover').addClass('panel-cover--collapsed')
+	
+  }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
